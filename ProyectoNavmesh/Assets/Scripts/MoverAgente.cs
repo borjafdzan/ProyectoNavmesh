23e03dbf162e03facab4df5cc06d471580a2ff5e
BorjaFdzAn <BorjaFdzAn@gmail.com>
2022-05-10 19:49:28 +0200

    Creacion del navmesh y recorrido

---
+++ ProyectoNavmesh/Assets/Scripts/MoverAgente.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class MoverAgente : MonoBehaviour
+{
+    public Transform[] posicionesMoverse;
+    private NavMeshAgent agente;
+    private int indicePatrulla;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.agente = GetComponent<NavMeshAgent>();
+        if (posicionesMoverse.Length == 0)
+        {
+            Debug.Log("No se han metido las posiciones correctamente");
+        }
+        else
+        {
+            this.agente.destination = posicionesMoverse[0].position;
+            this.indicePatrulla = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AlcanzoDestino()){
+            SiguientePosicion();
+        }
+    }
+    private bool AlcanzoDestino(){
+        if (Vector3.Distance(this.transform.position, posicionesMoverse[indicePatrulla].position) < 0.2f){
+            Debug.Log("Llego al destino");
+            return true;
+        }
+        return false;
+    }
+    private void SiguientePosicion(){
+        this.indicePatrulla++;
+        if (indicePatrulla >= this.posicionesMoverse.Length){
+            this.indicePatrulla = 0;
+        }
+        this.agente.destination = posicionesMoverse[indicePatrulla].position;
+    }
+}
