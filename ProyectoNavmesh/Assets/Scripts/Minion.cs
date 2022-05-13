66dd21432dff4aeb1be40f565c213e9a2f39bba8
BorjaFdzAn <BorjaFdzAn@gmail.com>
2022-05-13 20:37:25 +0200

    Deteccion de movimiento si esta cerca

--- ProyectoNavmesh/Assets/Scripts/Minion.cs
+++ ProyectoNavmesh/Assets/Scripts/Minion.cs
@@ -18,7 +18,7 @@ public class Minion : MonoBehaviour
     void Start()
     {
         this.agenteNavegacion = GetComponent<NavMeshAgent>();
-        StartCoroutine(DetectarJugador());
+        
         agenteNavegacion.destination = Jugador.transform.position;
         this.renderizador.material.SetColor("__Color", colorActivo);
     }
@@ -26,44 +26,17 @@ public class Minion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //agenteNavegacion.destination = Jugador.transform.position;
-    }
-
-    IEnumerator DetectarJugador()
-    {
-        while (true)
-        {
-            float distanciaJugador = 0;
-            RaycastHit golpeRayo;
-            RaycastHit[] JugadoresEnObjetivo = Physics.SphereCastAll(this.transform.position, 10, this.transform.forward, 10, mascaraJugador);
+        RaycastHit[] JugadoresEnObjetivo = Physics.SphereCastAll(this.transform.position, 10, this.transform.forward, 10, mascaraJugador);
             if (JugadoresEnObjetivo.Length > 0)
             {
-                Debug.Log("Se detecta el jugador");
-                
-                float angulo = ComprobarRango(JugadoresEnObjetivo[0].transform.gameObject);
-                if (angulo < 40){
-                    this.agenteNavegacion.destination = Jugador.transform.position;
-                }
-                this.renderizador.material.SetColor("__Color", colorActivo);
+                this.agenteNavegacion.destination = Jugador.transform.position;
             }
             else
             {
                 this.agenteNavegacion.destination = Casa.position;
                 this.renderizador.material.SetColor("__Color", colorDesactivo);
             }
-            yield return new WaitForSeconds(0.1f);
-        }
     }
 
-    //Este metodo deuelve el angulo de deteccion
-    private float  ComprobarRango(GameObject objetivoDetectado){
-        Vector3 direccion = this.transform.position - objetivoDetectado.transform.position;
-        direccion = direccion.normalized;
-        direccion = - direccion;
-        Debug.DrawLine(this.transform.position, objetivoDetectado.transform.position, Color.cyan, 1);
-        float angulo = Vector3.Angle(this.transform.forward, direccion);
-        Debug.Log("El angulo actual es de " + angulo);
-        return angulo;
-        //RaycastHit rayo = Physics.Raycast(this.transform.position, direccion, 10, mascaraJugador):
-    }
+    
 }
